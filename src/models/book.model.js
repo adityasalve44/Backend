0c959f9e23c90b77ef70183c385aa6f9c56430e7
adityasalve44 <aditya.salve.dev44@gmail.com>
2024-04-28 15:19:38 +0530

    Created User and Book model

---
+++ src/models/book.model.js
@@ -0,0 +1,56 @@
+import mongoose, { Schema } from "mongoose";
+
+const bookSchema = new Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		author: {
+			type: String,
+			required: true,
+		},
+		publicationYear: {
+			type: Number,
+			required: true,
+		},
+		printingYear: {
+			type: Number,
+			required: true,
+		},
+		genre: {
+			type: [String], // Array of genres
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		images: {
+			type: [String], // Array of image URLs
+			required: true,
+		},
+		pages: {
+			type: Number,
+			required: true,
+		},
+		condition: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+		uploadedBy: {
+			type: Schema.Types.ObjectId, // Assuming each book is uploaded by a user
+			ref: "User", // Reference to the User model
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+export const Book = mongoose.model("Book", bookSchema);
